ffdf8fdaae8ad44bee71ca5bf7b0b3ea2ef51033
oiiot <elliottmullins@btinternet.com>
2024-02-15 15:15:34 +0000

    added eventbridge role and rule

---
+++ Terraform/eventbridge.tf
@@ -0,0 +1,15 @@
+module "eventbridge" {
+  source = "terraform-aws-modules/eventbridge/aws"
+  attach_lambda_policy = true
+  lambda_target_arns   = [lambda arn goes here]
+
+  schedules = {
+    lambda-cron = {
+      description         = "Trigger for a Lambda"
+      schedule_expression = "cron(*/5 * * * ? *)"
+      timezone            = "Europe/London"
+      arn                 = [lambda arn goes here]
+      input               = jsonencode({ "job" : "cron-by-rate" })
+    }
+  }
+} 
