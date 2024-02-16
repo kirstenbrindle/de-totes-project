ae85447cb0f5a184a089bee6605798d70c798713
Tomroberts22 <tom_roberts22@hotmail.co.uk>
2024-02-16 10:36:04 +0000

    update most recent file function and add test

--- test/test_utils/test_get_most_recent_file.py
+++ test/test_utils/test_get_most_recent_file.py
@@ -29,5 +29,6 @@ def test_returns_file_name_in_folder():
     mockedClient.create_bucket(Bucket="test_bucket", CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
     mockedClient.put_object(Bucket="test_bucket", Key="folder1/test_file_1.txt", Body="test_string")
     mockedClient.put_object(Bucket="test_bucket", Key="folder1/test_file_2.txt", Body="test_string2")
+    mockedClient.put_object(Bucket="test_bucket", Key="folder2/test_file_3.txt", Body="test_string3")
     result = get_most_recent_file(mockedClient, "folder1")
-    assert result == "test_file_2.txt"
+    assert result == "folder1/test_file_2.txt"
