e1083a681aced124e2bf2ef21786516c925abb2c
minnie-manson <minniemanson@outlook.com>
2024-02-16 15:07:51 +0000

    add correct arns to eventbridge role

--- Terraform/eventbridge.tf
+++ Terraform/eventbridge.tf
@@ -1,14 +1,14 @@
 # module "eventbridge" {
 #   source = "terraform-aws-modules/eventbridge/aws"
 #   attach_lambda_policy = true
-#   lambda_target_arns   = [lambda arn goes here]
+#   lambda_target_arns   = aws_lambda_function.L1_injestion.arn
 
 #   schedules = {
 #     lambda-cron = {
 #       description         = "Trigger for a Lambda"
 #       schedule_expression = "cron(*/5 * * * ? *)"
 #       timezone            = "Europe/London"
-#       arn                 = [lambda arn goes here]
+#       arn                 = aws_lambda_function.L1_injestion.arn
 #       input               = jsonencode({ "job" : "cron-by-rate" })
 #     }
 #   }
