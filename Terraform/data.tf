cb5fa0881ddbe812d6e2f2cd128bf8e5b07c2513
Leah <leah@Leahs-iMac.local>
2024-02-20 15:38:07 +0000

    lambda1 dep layers added

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -4,9 +4,17 @@ data "aws_region" "current" {}
 
 data "archive_file" "lambda1" {
   type        = "zip"
-  source_file = "${path.module}/../src/lambda1/lambda1.py"
+  source_dir = "${path.module}/../src/lambda1"
   output_path = "${path.module}/../lambda1.zip"
 }
+
+# data "archive_file" "lambda1_layer" {
+#   type        = "zip"
+#   source_file  = "${path.module}/../src/lambda1/L1-layer/requirements.txt"
+#   output_path = "${path.module}/../lambda_layer.zip"
+#   # depends_on  = [ null_resource.pip_install ]
+# }
+
 
 # data "archive_file" "lambda2" {
 #   type        = "zip"
---
+++ Terraform/lambda1_layers.tf
@@ -0,0 +1,35 @@
+# #define variables
+# locals {
+#   layer_path        = "lambda1_layer"
+#   layer_zip_name    = "lambda1_layer.zip"
+#   layer_name        = "lambda1_dep_layer"
+#   requirements_name = "requirements.txt"
+#   requirements_path = "${path.module}/../src/lambda1/L1-layer/requirements.txt"
+# }
+
+# # create zip file from requirements.txt. Triggers only when the file is updated
+# resource "null_resource" "lambda_layer" {
+#   triggers = {
+#     requirements = filesha1(local.requirements_path)
+#   }
+#   # the command to install python and dependencies to the machine and zips
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       cd ${local.layer_path}
+#       rm -rf python
+#       mkdir python
+#       pip3 install -r ${local.requirements_name} -t python/
+#       zip -r ${local.layer_zip_name} python/
+#     EOT
+#   }
+# }
+
+
+# # create lambda layer
+# resource "aws_lambda_layer_version" "lambda_layer" {
+#   layer_name          = "lambda1-layer"
+#   filename            = data.archive_file.lambda1_layer.output_path
+#   source_code_hash    = data.archive_file.lambda1_layer.output_base64sha256
+#   compatible_runtimes = ["python3.10"]
+# #   depends_on          = [aws_s3_object.lambda_layer_zip] # triggered only if the zip file is uploaded to the bucket
+# }
