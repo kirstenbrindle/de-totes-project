a0b55672dd57c89516baa8270fccfe817106a9f8
minnie-manson <minniemanson@outlook.com>
2024-02-21 16:07:18 +0000

    change region handler

--- src/extract_handler1/extract_handler1.py
+++ src/extract_handler1/extract_handler1.py
@@ -8,7 +8,7 @@ from src.utils_lambda1.get_bucket_name import get_bucket_name
 from src.utils_lambda1.is_bucket_empty import is_bucket_empty
 from src.utils_lambda1.L1_extract_data import L1_extract_data
 
-secretm = boto3.client("secretsmanager")
+secretm = boto3.client("secretsmanager", region_name='eu-west-2')
 secret_file_name = secretm.get_secret_value(SecretId="totes_secret_aws")
 secrets_dict = json.loads(secret_file_name["SecretString"])
 
@@ -35,7 +35,7 @@ def lambda_handler(event, context):
     '''
     try:
         conn = Connection(**secrets_dict)
-        s3 = boto3.client('s3')
+        s3 = boto3.client('s3', region_name='eu-west-2')
         table_names = get_table_names(conn)
         bucket_name = get_bucket_name()
         boolean = is_bucket_empty(bucket_name, s3)
