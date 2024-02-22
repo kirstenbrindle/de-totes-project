94650dc62505625809efa9e27df0511bc443ef0e
Tomroberts22 <tom_roberts22@hotmail.co.uk>
2024-02-22 12:29:39 +0000

    add comments to lambda handler 2

--- src/transform_handler2/transform_handler2.py
+++ src/transform_handler2/transform_handler2.py
@@ -11,4 +11,20 @@ def lambda_handler(event, context):
         RuntimeError: An unexpected error occurred in execution. Other errors
         result in an informative log message.
     '''
+    # setup s3 client = s3
+    # file_name = get_key_name(event["Records"])
+    # process_bucket_name = get_bucket_name(s3)
+    # boolean = is_bucket_empty(s3, process_bucket_name)
+    # if boolean is true - get_most_recent_file() - Variables for each - sales_order, design, address, currency, staff, department, counterparty and should be saved as variables.
+    # dataframes = read_csv_to_df(Variables for each - sales_order, design, address, currency, staff, department, counterparty)
+    # Invoke each make_dim format function and pass in needed dataframes. 
+    # Output of dim format functions are passed into write_to_parquet function
+
+    # If boolean is False
+    # Use filename from get_key_name - read the file using read_csv_to_df(file_name) - saved as a dataframe.
+    # if file_name contains sales_order - invoke make_dim_sales_order() formatter func.
+    #  if file_name contains design - invoke make_dim_design() formatter func.
+    # Output of dim sales order or dim design passed into write_to_parquet function.
+
+
     pass
