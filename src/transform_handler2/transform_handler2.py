2d979051af8037b8082c9b3b37a5c7c73c6197f0
kirstenbrindle <kirstenbrindle@outlook.com>
2024-02-22 14:18:05 +0000

    update read_csv func to add s3 connection

--- src/transform_handler2/transform_handler2.py
+++ src/transform_handler2/transform_handler2.py
@@ -15,16 +15,19 @@ def lambda_handler(event, context):
     # file_name = get_key_name(event["Records"])
     # process_bucket_name = get_bucket_name(s3)
     # boolean = is_bucket_empty(s3, process_bucket_name)
-    # if boolean is true - get_most_recent_file() - Variables for each - sales_order, design, address, currency, staff, department, counterparty and should be saved as variables.
-    # dataframes = read_csv_to_df(Variables for each - sales_order, design, address, currency, staff, department, counterparty)
-    # Invoke each make_dim format function and pass in needed dataframes. 
+    # if boolean is true - get_most_recent_file() -
+    # Variables for each - sales_order, design, address, currency,
+    # staff, department, counterparty and should be saved as variables.
+    # dataframes = read_csv_to_df(Variables for each - sales_order, design,
+    # address, currency, staff, department, counterparty)
+    # Invoke each make_dim format function and pass in needed dataframes.
     # Output of dim format functions are passed into write_to_parquet function
 
     # If boolean is False
-    # Use filename from get_key_name - read the file using read_csv_to_df(file_name) - saved as a dataframe.
-    # if file_name contains sales_order - invoke make_dim_sales_order() formatter func.
+    # Use filename from get_key_name - read file with read_csv_to_df(file_name)
+    # - saved as a dataframe.
+    # if file_name contains sales_order - invoke make_dim_sales_order().
     #  if file_name contains design - invoke make_dim_design() formatter func.
-    # Output of dim sales order or dim design passed into write_to_parquet function.
-
+    # Output of dim sales order or dim design passed into write_to_parquet.
 
     pass
--- test/test_utils_lambda2/test_read_csv_to_df.py
+++ test/test_utils_lambda2/test_read_csv_to_df.py
@@ -3,24 +3,56 @@ from unittest.mock import patch
 import pytest
 import pandas as pd
 import logging
+import os
+from moto import mock_aws
+import boto3
 
 logger = logging.getLogger('test')
 logger.setLevel(logging.INFO)
 logger.propagate = True
 
 
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto"""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture
+def mock_s3(aws_credentials):
+    with mock_aws():
+        yield boto3.client("s3", region_name='eu-west-2')
+
+
+@pytest.fixture
+def mock_bucket(mock_s3):
+    mock_s3.create_bucket(
+        Bucket='test-bucket',
+        CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
+
+
 @pytest.mark.describe("read_csv_to_df")
 @pytest.mark.it("test pandas read method is invoked")
+@mock_aws
 @patch("src.transform_handler2.read_csv_to_df.pd")
-def test_reads_method_is_invoked(mock_pd):
+def test_reads_method_is_invoked(mock_pd, mock_s3, mock_bucket):
     assert mock_pd.read_csv.call_count == 0
-    read_csv_to_df("test1.csv")
+    mock_s3.upload_file('test/test_csv_files/test1.csv',
+                        'test-bucket', 'test/test1.csv')
+    read_csv_to_df(mock_s3, 'test-bucket', "test/test1.csv")
     assert mock_pd.read_csv.call_count == 1
 
 
 @pytest.mark.describe("read_csv_to_df")
 @pytest.mark.it("test pandas returns dataframe with file contents")
-def test_returns_dataframe():
+@mock_aws
+def test_returns_dataframe(mock_s3, mock_bucket):
+    mock_s3.upload_file('test/test_csv_files/test1.csv',
+                        'test-bucket', 'test/test1.csv')
     data = {
         'column1': ['r1c1', 'r2c1', 'r3c1'],
         'column2': ['r1c2', 'r2c2', 'r3c2'],
@@ -28,21 +60,25 @@ def test_returns_dataframe():
     }
     row_labels = [0, 1, 2]
     expected = pd.DataFrame(data=data, index=row_labels)
-    result = read_csv_to_df("test/test_csv_files/test1.csv")
+    result = read_csv_to_df(mock_s3, 'test-bucket', "test/test1.csv")
     assert result.equals(expected)
 
 
 @pytest.mark.describe("read_csv_to_df")
 @pytest.mark.it("test error output when given incorrect file path to read")
-def test_logs_error_when_passed_incorrect_file_path(caplog):
+@mock_aws
+def test_logs_error_when_passed_incorrect_file_path(
+        mock_s3, mock_bucket, caplog):
     with caplog.at_level(logging.INFO):
-        read_csv_to_df("test/test_csv_files/test4.csv")
-        assert "Specified file cannot be found" in caplog.text
+        read_csv_to_df(mock_s3, 'test-bucket', "test/test_csv_files/test4.csv")
+        assert "The specified key does not exist." in caplog.text
 
 
 @pytest.mark.describe("read_csv_to_df")
 @pytest.mark.it("test error output when given incorrect file type")
-def test_logs_error_when_passed_incorrect_file_type(caplog):
+@mock_aws
+def test_logs_error_when_passed_incorrect_file_type(
+        mock_s3, mock_bucket, caplog):
     with caplog.at_level(logging.INFO):
-        read_csv_to_df("requirements.txt")
+        read_csv_to_df(mock_s3, 'test-bucket', "requirements.txt")
         assert "File type incorrect, must be csv format" in caplog.text
