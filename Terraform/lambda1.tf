f9a3979cc2340dced51ecf5ea6a7a8c3c72e2e15
minnie-manson <minniemanson@outlook.com>
2024-02-22 17:09:51 +0000

    update for deployment (layers)

--- Terraform/lambda1.tf
+++ Terraform/lambda1.tf
@@ -1,19 +1,24 @@
 # L1 lambda function resource
 
 resource "aws_lambda_function" "lambda1" {
-  function_name = var.lambda1
-  role          = aws_iam_role.L1_lambda_role.arn
-  handler       = "extract_handler1.lambda_handler"
-  runtime       = "python3.10"
-  s3_bucket     = aws_s3_bucket.code_bucket.id
-  s3_key        = "lambda1/lambda1.zip"
+  function_name    = var.lambda1
+  role             = aws_iam_role.L1_lambda_role.arn
+  handler          = "extract_handler1.lambda_handler"
+  runtime          = "python3.10"
+  s3_bucket        = aws_s3_bucket.code_bucket.id
+  s3_key           = "lambda1/lambda1.zip"
   source_code_hash = data.archive_file.lambda1.output_base64sha256
   environment {
     variables = {
       S3_ingestion_bucket = "${aws_s3_bucket.ingestion_bucket.id}"
     }
   }
-  layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python311:8", aws_lambda_layer_version.lambda_layer.arn]
+  layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python310-Arm64:11", aws_lambda_layer_version.lambda_layer_dependencies.arn] 
+  # we have two layers:
+  # - first is a aws made pandas layer - we need specifically python310 and Arm64
+  # - second is our layer containing depedencies like pg8000 and all our utility
+  #   functions. This is so we can still import like ' from src.extract_handler..
+  #   etc which means our tests will still run!
 }
 
 
@@ -26,3 +31,13 @@ resource "aws_lambda_permission" "allow_eventbridge" {
   source_account = data.aws_caller_identity.current.account_id
 }
 
+resource "aws_lambda_layer_version" "lambda_layer_dependencies" {
+  layer_name          = "lambda_layer1"
+  s3_bucket           = aws_s3_bucket.code_bucket.id
+  s3_key              = aws_s3_object.lambda_layer_zip.key
+  compatible_runtimes = ["python3.10"]
+  source_code_hash    = base64sha256(filebase64("${path.module}/../aws_utils/layer_code1.zip"))
+  # We have included a source code hash so that everytime there's an update
+  # to our layer_code1.zip file i.e. if we have a new dependency to add
+  # it updates automatically.
+}
---
+++ aws_utils/layer_code1/python/src/extract_handler1/is_bucket_empty.py
@@ -0,0 +1,19 @@
+def is_bucket_empty(bucket_name, s3):
+    """
+    This function checks if the bucket is empty and return a boolean
+
+    Arg:
+
+    `bucket_name`: string of the bucket name
+
+    ---------------------------
+    Returns:
+
+    `boolean`: True if bucket is empty, false otherwise
+
+
+    """
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    if response['KeyCount'] == 0:
+        return True
+    return False
