1a7545dd5a523687e70a2eb36f398ecb8446d163
Tomroberts22 <tom_roberts22@hotmail.co.uk>
2024-02-22 10:45:39 +0000

    update read csv func

---
+++ src/transform_handler2/read_csv_to_df.py
@@ -0,0 +1,19 @@
+
+
+def read_csv_to_df(file_name):
+        """
+    This function reads a csv file and returns the contents as a dataframe.
+
+    Arg:
+
+    `file_name`: string of the csv file name
+
+    ---------------------------
+    Returns:
+
+    `dataframe`: a dataframe
+
+
+    """
+        pass
+
---
+++ test/test_utils_lambda2/test_read_csv_to_df.py
@@ -0,0 +1,4 @@
+from src.transform_handler2.read_csv_to_df import read_csv_to_df
+
+@pytest.mark.describe("read_csv_to_df")
+@pytest.mark.it("")
