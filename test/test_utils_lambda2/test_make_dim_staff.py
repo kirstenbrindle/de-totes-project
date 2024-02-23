53d85570e027711e443a55eae39a053b54e7bf5f
minnie-manson <minniemanson@outlook.com>
2024-02-23 15:52:15 +0000

    add make dim staff and tests (passing)

--- test/test_utils_lambda2/test_make_dim_staff.py
+++ test/test_utils_lambda2/test_make_dim_staff.py
@@ -2,6 +2,7 @@ from src.transform_handler2.make_dim_staff import make_dim_staff
 import pytest
 import pandas as pd
 
+
 @pytest.mark.describe("make_dim_staff")
 @pytest.mark.it("Test returns a dataframe")
 def test_returns_a_dataframe():
@@ -46,7 +47,7 @@ def test_returns_a_filtered_and_merged_dataframe():
         'staff_id': ['1', '2', '3'],
         'first_name': ['Tom', 'Kirsten', 'Cinthya'],
         'last_name': ['Roberts', 'Robertson', 'Jones'],
-        'department_id': ['1', '2', '3'],
+        'department_id': ['d1', 'd2', 'd3'],
         'email_address': ['a', 'b', 'c'],
         'created_at': ['3', '6', '9'],
         'last_updated': ['10', '11', '12']
@@ -54,8 +55,8 @@ def test_returns_a_filtered_and_merged_dataframe():
     row_labels = [1, 2, 3]
     df = pd.DataFrame(data=data_staff, index=row_labels)
     data_department = {
-        'department_id': ['1', '2', '3'],
-        'department_name': ['z', 'f', 'g'],
+        'department_id': ['d1', 'd2', 'd3'],
+        'department_name': ['health', 'food', 'tech'],
         'location': ['Manchester', 'Chester', 'Liverpool'],
         'manager': ['Manager', 'Manager1', 'Manager2'],
         'created_at': ['3', '6', '9'],
@@ -67,15 +68,17 @@ def test_returns_a_filtered_and_merged_dataframe():
         'staff_id': ['1', '2', '3'],
         'first_name': ['Tom', 'Kirsten', 'Cinthya'],
         'last_name': ['Roberts', 'Robertson', 'Jones'],
-        'department_name': ['z', 'f', 'g'],
+        'department_name': ['health', 'food', 'tech'],
         'location': ['Manchester', 'Chester', 'Liverpool'],
         'email_address': ['a', 'b', 'c']
     }
-    row_labels = [1, 2, 3]
+    row_labels = [0, 1, 2]
     df_dim_staff = pd.DataFrame(data=data_dim_staff, index=row_labels)
 
     result = make_dim_staff(df, df_2)
+    print(df_dim_staff)
     assert result.equals(df_dim_staff)
+
 
 @pytest.mark.describe("make_dim_staff")
 @pytest.mark.it("Test returns a filtered and merged dataframe when "
@@ -110,7 +113,7 @@ def test_returns_a_filtered_and_merged_dataframe_when_table_order_is_irregular()
         'location': ['Liverpool', 'Chester', 'Manchester'],
         'email_address': ['a', 'b', 'c']
     }
-    row_labels = [1, 2, 3]
+    row_labels = [0, 1, 2]
     df_dim_staff = pd.DataFrame(data=data_dim_staff, index=row_labels)
 
     result = make_dim_staff(df, df_2)
