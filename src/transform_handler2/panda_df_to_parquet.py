56d4c3ec00d802352cc958e60d42bbc3f0f01207
kirstenbrindle <kirstenbrindle@outlook.com>
2024-02-23 09:21:53 +0000

    update variable name panda_to_parquet

--- src/transform_handler2/panda_df_to_parquet.py
+++ src/transform_handler2/panda_df_to_parquet.py
@@ -7,21 +7,21 @@ import io
 logger = logging.getLogger('lambda2Logger')
 logger.setLevel(logging.INFO)
 
-def csv_parquet_converter(s3_client, bucket_name, table_name, data):
+def csv_parquet_converter(s3_client, bucket_name, olap_table, data):
     table = pa.Table.from_pandas(data)
     file = io.BytesIO()
     papq.write_table(table, file)
     s3_client.put_object(
         Bucket= bucket_name,
         Body= file.getvalue(),
-        Key= f'{table_name}/{table_name}-{datetime.now()}.parquet'
+        Key= f'{olap_table}/{olap_table}-{datetime.now()}.parquet'
     )
     
 '''
 takes 4 arguments:
 the s3 conenction,
 the name of the bucket you want it to save in,
-the name of the table you're converting
+the name of the OLAP table the new data is for
 and the dataframe as a variable
 
 converts 
