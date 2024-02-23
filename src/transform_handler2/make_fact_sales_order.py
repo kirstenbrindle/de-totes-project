6ccbde49692cbcdf9e865ba5828d2f9b95faaea9
Tomroberts22 <tom_roberts22@hotmail.co.uk>
2024-02-23 17:22:53 +0000

    update fact sales

--- src/transform_handler2/make_fact_sales_order.py
+++ src/transform_handler2/make_fact_sales_order.py
@@ -1,2 +1,8 @@
-def make_fact_sales_order():
-    pass
+def make_fact_sales_order(df):
+    return df 
+
+
+# 3 additional rows
+# 2 are splits of created at and last updated timestamps
+# third is serial primary key of sales record_ID
+# add prefix on to staff_id
