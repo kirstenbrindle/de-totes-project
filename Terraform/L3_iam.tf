443df179446b63e4681b1819f555602ccecaa17a
minnie-manson <minniemanson@outlook.com>
2024-02-26 12:02:00 +0000

    add sns alerts for L2

--- Terraform/L3_iam.tf
+++ Terraform/L3_iam.tf
@@ -1,23 +1,23 @@
-# creating secretsmanager policy doc...
-data "aws_iam_policy_document" "wh_access_doc" {
-  statement {
-    effect = "Allow"
+# # creating secretsmanager policy doc...
+# data "aws_iam_policy_document" "wh_access_doc" {
+#   statement {
+#     effect = "Allow"
 
-  principals {
-      type        = "AWS"
-      identifiers = ["LAMBDA3 ARN"]
-    }
+#   principals {
+#       type        = "AWS"
+#       identifiers = ["LAMBDA3 ARN"]
+#     }
 
-    actions   = ["secretsmanager:GetSecretValue"]
-    resources = ["arn:aws:secretsmanager:eu-west-2:533267264466:secret:warehouseCredentials-ZMya4g"]
-  }
-}
+#     actions   = ["secretsmanager:GetSecretValue"]
+#     resources = ["arn:aws:secretsmanager:eu-west-2:533267264466:secret:warehouseCredentials-ZMya4g"]
+#   }
+# }
 
-#creating secretmanager policy...
-resource "aws_secretsmanager_secret_policy" "wh_access_policy" {
-  secret_arn = "arn:aws:secretsmanager:eu-west-2:533267264466:secret:warehouseCredentials-ZMya4g"
-  policy     = data.aws_iam_policy_document.wh_access_doc.json
-}
+# #creating secretmanager policy...
+# resource "aws_secretsmanager_secret_policy" "wh_access_policy" {
+#   secret_arn = "arn:aws:secretsmanager:eu-west-2:533267264466:secret:warehouseCredentials-ZMya4g"
+#   policy     = data.aws_iam_policy_document.wh_access_doc.json
+# }
 
 # # Attaching SecretManager Policy to L3 IAM Role ...
 # resource "aws_iam_role_policy_attachment" "wh_access_policy_attachment" {
