6ece81a2f3b509b19be021fb06df64f67759020d
kirstenbrindle <kirstenbrindle@outlook.com>
2024-02-27 12:34:42 +0000

    add terraform L3 layer code changes

--- Terraform/L3_iam.tf
+++ Terraform/L3_iam.tf
@@ -1,130 +1,112 @@
-# # # creating secretsmanager policy doc...
-# # data "aws_iam_policy_document" "wh_access_doc" {
-# #   statement {
-# #     effect = "Allow"
+data "aws_iam_policy_document" "wh_access_doc" {
+  statement {
+    effect = "Allow"
 
-# #   principals {
-# #       type        = "AWS"
-# #       identifiers = ["LAMBDA3 ARN"]
-# #     }
+  principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:sts::533267264466:assumed-role/L3_lambda_role/load_handler3"]
+    }
 
-# #     actions   = ["secretsmanager:GetSecretValue"]
-# #     resources = ["arn:aws:secretsmanager:eu-west-2:533267264466:secret:warehouseCredentials-ZMya4g"]
-# #   }
-# # }
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["arn:aws:secretsmanager:eu-west-2:533267264466:secret:warehouseCredentials-ZMya4g"]
+  }
+}
 
-# # #creating secretmanager policy...
-# # resource "aws_secretsmanager_secret_policy" "wh_access_policy" {
-# #   secret_arn = "arn:aws:secretsmanager:eu-west-2:533267264466:secret:warehouseCredentials-ZMya4g"
-# #   policy     = data.aws_iam_policy_document.wh_access_doc.json
-# # }
+resource "aws_secretsmanager_secret_policy" "wh_access_policy" {
+  secret_arn = "arn:aws:secretsmanager:eu-west-2:533267264466:secret:warehouseCredentials-ZMya4g"
+  policy     = data.aws_iam_policy_document.wh_access_doc.json
+}
 
-# # # Attaching SecretManager Policy to L3 IAM Role ...
-# # resource "aws_iam_role_policy_attachment" "wh_access_policy_attachment" {
-# #   role       = aws_iam_role.L3_lambda_role.name
-# #   policy_arn = aws_secretsmanager_secret_policy.wh_access_policy.arn
-# # }
+resource "aws_iam_role" "L3_lambda_role" {
+  name = "L3_lambda_role"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "sts:AssumeRole"
+          ],
+          "Principal" : {
+            "Service" : [
+              "lambda.amazonaws.com"
+            ]
+          }
+        }
+      ]
+  })
+}
 
+data "aws_iam_policy_document" "s3_L3_document" {
+  statement {
+    actions = [
+      "s3:GetBucket"
+    ]
+    resources = [
+      "${aws_s3_bucket.processed_bucket.arn}",
+      "${aws_s3_bucket.code_bucket.arn}"
+    ]
+    effect = "Allow"
+  }
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.processed_bucket.arn}/*",
+      "${aws_s3_bucket.code_bucket.arn}/*"
+    ]
+    effect = "Allow"
+  }
+  statement {
+    actions = [
+      "s3:ListAllMyBuckets"
+    ]
+    resources = [
+      "arn:aws:s3:::*"
 
-# # Creating L3 IAM Role...
-# resource "aws_iam_role" "L3_lambda_role" {
-#   name = "L3_lambda_role"
-#   assume_role_policy = jsonencode(
-#     {
-#       "Version" : "2012-10-17",
-#       "Statement" : [
-#         {
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "sts:AssumeRole"
-#           ],
-#           "Principal" : {
-#             "Service" : [
-#               "lambda.amazonaws.com"
-#             ]
-#           }
-#         }
-#       ]
-#   })
-# }
+    ]
+    effect = "Allow"
+  }
+}
 
-# # Creating S3 Processed bucket policy document ...
-# data "aws_iam_policy_document" "s3_L3_document" {
-#   statement {
-#     actions = [
-#       "s3:GetBucket"
-#     ]
-#     resources = [
-#       "${aws_s3_bucket.processed_bucket.arn}",
-#       "${aws_s3_bucket.code_bucket.arn}"
-#     ]
-#     effect = "Allow"
-#   }
-#   statement {
-#     actions = [
-#       "s3:GetObject"
-#     ]
-#     resources = [
-#       "${aws_s3_bucket.processed_bucket.arn}/*",
-#       "${aws_s3_bucket.code_bucket.arn}/*"
-#     ]
-#     effect = "Allow"
-#   }
-#   statement {
-#     actions = [
-#       "s3:ListAllMyBuckets"
-#     ]
-#     resources = [
-#       "arn:aws:s3:::*"
+resource "aws_iam_policy" "s3_L3_policy" {
+  name_prefix = "s3-L3-policy-de-totes-project-"
+  policy      = data.aws_iam_policy_document.s3_L3_document.json
+}
 
-#     ]
-#     effect = "Allow"
-#   }
-# }
+resource "aws_iam_role_policy_attachment" "L3_S3_policy_attachment" {
+  role       = aws_iam_role.L3_lambda_role.name
+  policy_arn = aws_iam_policy.s3_L3_policy.arn
+}
 
-# # Creating S3 Processed bucket policy ...
-# resource "aws_iam_policy" "s3_L3_policy" {
-#   name_prefix = "s3-L3-policy-de-totes-project-"
-#   policy      = data.aws_iam_policy_document.s3_L3_document.json
-# }
+data "aws_iam_policy_document" "L3_cloudwatch_document" {
+  statement {
+    actions = ["logs:CreateLogGroup"]
 
-# # Attaching S3 Processed policy to L3 IAM Role ...
-# resource "aws_iam_role_policy_attachment" "L3_S3_policy_attachment" {
-#   role       = aws_iam_role.L3_lambda_role.name
-#   policy_arn = aws_iam_policy.s3_L3_policy.arn
-# }
+    resources = [
+      "arn:aws:logs:eu-west-2:${data.aws_caller_identity.current.account_id}:*"
+    ]
 
-# #Creating Cloudwatch policy document for L3...
-# data "aws_iam_policy_document" "L3_cloudwatch_document" {
-#   statement {
-#     actions = ["logs:CreateLogGroup"]
+    effect = "Allow"
+  }
 
-#     resources = [
-#       "arn:aws:logs:eu-west-2:${data.aws_caller_identity.current.account_id}:*"
-#     ]
+  statement {
+    actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
 
-#     effect = "Allow"
-#   }
+    resources = [
+      "arn:aws:logs:eu-west-2:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.lambda3}:*"
+    ]
+  }
+}
 
-#   statement {
-#     actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
+resource "aws_iam_policy" "L3_cloudwatch_policy" {
+  name_prefix = "L3-cloudwatch-policy-de-totes-project-"
+  policy      = data.aws_iam_policy_document.L3_cloudwatch_document.json
+}
 
-#     resources = [
-#       "arn:aws:logs:eu-west-2:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.lambda3}:*"
-#     ]
-#   }
-# }
-
-# #posibly add put objec permission for data warehouse??
-
-# # Creating Cloudwatch policy ...
-# resource "aws_iam_policy" "L3_cloudwatch_policy" {
-#   name_prefix = "L3-cloudwatch-policy-de-totes-project-"
-#   policy      = data.aws_iam_policy_document.L3_cloudwatch_document.json
-# }
-
-# # Attaching Cloudwatch Policy to L3 IAM Role ...
-# resource "aws_iam_role_policy_attachment" "L3_cloudwatch_policy_attachment" {
-#   role       = aws_iam_role.L3_lambda_role.name
-#   policy_arn = aws_iam_policy.L3_cloudwatch_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "L3_cloudwatch_policy_attachment" {
+  role       = aws_iam_role.L3_lambda_role.name
+  policy_arn = aws_iam_policy.L3_cloudwatch_policy.arn
+}
--- Terraform/l3_alarm.tf
+++ Terraform/l3_alarm.tf
@@ -1,25 +1,25 @@
-# resource "aws_cloudwatch_log_metric_filter" "error_log3" {
-#   name           = "Lambda3error"
-#   pattern        = "ERROR"
-#   log_group_name = "/aws/lambda/${var.lambda3}" 
+resource "aws_cloudwatch_log_metric_filter" "error_log3" {
+  name           = "Lambda3error"
+  pattern        = "ERROR"
+  log_group_name = "/aws/lambda/${var.lambda3}" 
 
-#   metric_transformation {
-#     name      = "ErrorCount"
-#     namespace = "lambda3_error"
-#     value     = "1"
-#   }
-# }
+  metric_transformation {
+    name      = "ErrorCount"
+    namespace = "lambda3_error"
+    value     = "1"
+  }
+}
 
-# resource "aws_cloudwatch_metric_alarm" "alert_errors_L3" {
-#   alarm_name          = "ErrorAlarmL3"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = 1
-#   metric_name         = aws_cloudwatch_log_metric_filter.error_log3.metric_transformation[0].name
-#   namespace           = aws_cloudwatch_log_metric_filter.error_log3.metric_transformation[0].namespace
-#   period              = 60
-#   statistic           = "Sum"
-#   alarm_description   = "This metric monitors error instances for L3: load_handler3"
-#   actions_enabled     = "true"
-#   alarm_actions       = ["arn:aws:sns:eu-west-2:533267264466:bitme-error-alerts"]
-#   threshold           = 1
-# }
+resource "aws_cloudwatch_metric_alarm" "alert_errors_L3" {
+  alarm_name          = "ErrorAlarmL3"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = aws_cloudwatch_log_metric_filter.error_log3.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.error_log3.metric_transformation[0].namespace
+  period              = 60
+  statistic           = "Sum"
+  alarm_description   = "This metric monitors error instances for L3: load_handler3"
+  actions_enabled     = "true"
+  alarm_actions       = ["arn:aws:sns:eu-west-2:533267264466:bitme-error-alerts"]
+  threshold           = 1
+}
