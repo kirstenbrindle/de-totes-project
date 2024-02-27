e140c2be76f375f9f3831ab3b92ab865c87cc68d
hhcindyhh <mc.sanchezga@gmail.com>
2024-02-27 16:57:48 +0000

    Add update_dim_date

--- test/test_utils_lambda2/test_make_dim_date.py
+++ test/test_utils_lambda2/test_make_dim_date.py
@@ -1,6 +1,7 @@
 from src.transform_handler2.make_dim_date import make_dim_date
 import pytest
 import pandas as pd
+import datetime
 
 
 @pytest.mark.describe("make_dim_date")
@@ -24,11 +25,30 @@ def test_returns_correct_columns():
 def test_returns_correct_column_types():
     columns_int = ['year', 'month', 'day',
                    'day_of_week', 'quarter']
-    columns_varchar = ['day_name', 'month_name']
+    columns_varchar = ['day_name', 'month_name','date_id']
     result = make_dim_date(start='2022-11-01', end='2032-12-31')
     types = result.dtypes
     for col in columns_int:
         assert types[col] == 'int32'
     for col in columns_varchar:
         assert types[col] == 'object'
-    assert types['date_id'] == 'datetime64[ns]'
+
+
+
+@pytest.mark.describe("make_dim_date")
+@pytest.mark.it("Test returns correct contents")
+def test_returns_correct_contents():
+
+    result = make_dim_date(start='2024-02-27', end='2024-02-27')
+    expected = {'date_id':[datetime.date(2024,2,27)],
+                'year':[2024], 
+                'month':[2],
+                'day':[27],
+                'day_of_week':[1],
+                'day_name':['Tuesday'],
+                'month_name':['February'],
+                'quarter':[1]}
+    
+    expected_df=pd.DataFrame(data=expected)
+    assert result['date_id'].equals(expected_df['date_id'])
+   
