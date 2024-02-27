c4f0cf3f1fa5f176716611e10eabaa3eacbcb3a3
Tomroberts22 <tom_roberts22@hotmail.co.uk>
2024-02-27 11:04:05 +0000

    get file and processed bucket name update and create tests

---
+++ src/load_handler3/get_file_and_processed_bucket_name.py
@@ -0,0 +1,22 @@
+
+
+def get_file_and_processed_bucket_name(records):
+    """
+     This function extracts the bucket name and\n
+     object from the records\n
+
+     Args:
+     `records`: new events.
+
+     ---------------------------
+     Returns:
+
+     `bucket name`: The bucket name of new the event.
+     `object file`: The file name of new the event.
+
+     """
+    file_name = records[0]['s3']['object']['key']
+    bucket_name = records[0]['s3']['bucket']['name']
+    file_name = file_name.replace('+', ' ')
+    file_name = file_name.replace('%', ':')
+    return f'{bucket_name}', f'{file_name}'
---
+++ test/test_utils_lambda3/test_get_file_and_processed_bucket_name.py
@@ -0,0 +1,47 @@
+from src.load_handler3.get_file_and_processed_bucket_name import (
+    get_file_and_processed_bucket_name)
+import pytest
+
+
+@pytest.mark.describe("get_the_file_name")
+@pytest.mark.it("Test returns correct processed bucket name")
+def test_get_file_name_return_bucket_name_from_processed_bucket():
+    event = {
+        "Records": [
+            {
+                "s3": {
+                    "bucket": {
+                        "name": "test-bucket"
+                    },
+                    "object": {
+                        "key": "testtable/currency/2022-2-27+11%00.parquet"
+                    }
+                }
+            }
+        ],
+    }
+    expected = 'test-bucket'
+    result = get_file_and_processed_bucket_name(event['Records'])
+    assert result[0] == expected
+
+
+@pytest.mark.describe("get_the_file_name")
+@pytest.mark.it("Test returns correct file name")
+def test_get_file_name_returns_correct_processed_file_name():
+    event = {
+        "Records": [
+            {
+                "s3": {
+                    "bucket": {
+                        "name": "test-bucket"
+                    },
+                    "object": {
+                        "key": "testtable/currency/2022-2-27+11%00.parquet"
+                    }
+                }
+            }
+        ],
+    }
+    expected = 'testtable/currency/2022-2-27 11:00.parquet'
+    result = get_file_and_processed_bucket_name(event['Records'])
+    assert result[1] == expected
